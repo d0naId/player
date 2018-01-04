fb6b64f7475281130d3c3c70d0be921b1d63fe59
Yuriy <Rep.yurii@gmail.com>
2018-01-04 21:03:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-token = '474968444:AAH6zF23ZO-0amCwMNH3wAnUTZxRN9lCGaY'
+token = '4******************' # пин бота
 
 database_name = 'music_all_list.db'  # Файл с базой данных
 shelve_name = 'shelve.db'  # Файл с хранилищем
